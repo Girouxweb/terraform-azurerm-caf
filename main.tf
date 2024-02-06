0744d0eb275a79eee0be8347355c5de549fd8cfb
Chris Giroux <chris.giroux@coretek.com>
2024-02-05 21:58:59 -0500

    update aztfmod/azurecaf provider to version = "2.0.0-preview3"

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "~> 1.2.0"
+      version = "2.0.0-preview3"
     }
     null = {
       source = "hashicorp/null"
